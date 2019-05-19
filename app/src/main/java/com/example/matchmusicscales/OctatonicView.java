59bd11838d00584e989f302c6090593ab4341dfc
Sakari Angervuori <sakari.angervuoris@kotiposti.net>
2019-05-19 12:39:14 +0300

    lisäyksiä

--- app/src/main/java/com/example/matchmusicscales/OctatonicView.java
+++ app/src/main/java/com/example/matchmusicscales/OctatonicView.java
@@ -1,15 +1,28 @@
 package com.example.matchmusicscales;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class OctatonicView extends Activity {
+
+public class OctatonicView extends ListActivity {
+
+    static final String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
+            "Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
+            "Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple" };
 
     @Override
-    public void onCreate(Bundle octatonic) {
-        setContentView(R.layout.octatonic_scales);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-        super.onCreate(octatonic);
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.octatonic_scales,FRUITS));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
 
     }
+
 }
